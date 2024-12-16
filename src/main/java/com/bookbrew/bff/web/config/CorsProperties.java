01ce16a28a9d852a3b24d4b5095e66da4f73f56b
Publio Buiatti Guirelli <publiotester@gmail.com>
2024-12-16 19:40:39 -0300

    :recycle: refactor: Adjust properties 'auth' and 'cors' in yaml file

---
+++ src/main/java/com/bookbrew/bff/web/config/CorsProperties.java
@@ -0,0 +1,18 @@
+package com.bookbrew.bff.web.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "cors")
+public class CorsProperties {
+    private String allowedOrigins;
+
+    public String getAllowedOrigins() {
+        return allowedOrigins;
+    }
+
+    public void setAllowedOrigins(String allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+}
