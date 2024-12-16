11f75eafd1e2c0d13912cf79c825264af03c8afa
Publio Buiatti Guirelli <publiotester@gmail.com>
2024-12-16 19:22:54 -0300

    :sparkles: feat: Implement initial bff integration with authentication service

---
+++ src/main/java/com/bookbrew/bff/web/BookBrewWebBffApplication.java
@@ -0,0 +1,13 @@
+package com.bookbrew.bff.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBrewWebBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookBrewWebBffApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bookbrew/bff/web/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.bookbrew.bff.web.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bookbrew.bff.web.client.AuthServiceClient;
+import com.bookbrew.bff.web.dto.ForgotPasswordRequestDTO;
+import com.bookbrew.bff.web.dto.LoginRequestDTO;
+import com.bookbrew.bff.web.dto.ResetPasswordRequestDTO;
+
+@RestController
+@RequestMapping("/bff/auth")
+@CrossOrigin(origins = "${cors.allowed-origins}")
+public class AuthController {
+
+    private final AuthServiceClient authServiceClient;
+
+    public AuthController(AuthServiceClient authServiceClient) {
+        this.authServiceClient = authServiceClient;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequestDTO request) {
+        return authServiceClient.login(request);
+    }
+
+    @PostMapping("/forgot-password")
+    public ResponseEntity<?> forgotPassword(@RequestBody ForgotPasswordRequestDTO request) {
+        return authServiceClient.forgotPassword(request);
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<?> resetPassword(@RequestBody ResetPasswordRequestDTO request) {
+        return authServiceClient.resetPassword(request);
+    }
+}
