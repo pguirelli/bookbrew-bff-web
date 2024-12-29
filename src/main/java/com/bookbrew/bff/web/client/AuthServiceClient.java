afcc7687ab901a4c8663bd6dc15f6fe246a04469
Publio Buiatti Guirelli <publiotester@gmail.com>
2024-12-29 19:42:55 -0300

    :recycle: refactor: Adjustments for the first version of the front

--- src/main/java/com/bookbrew/bff/web/client/AuthServiceClient.java
+++ src/main/java/com/bookbrew/bff/web/client/AuthServiceClient.java
@@ -7,19 +7,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import com.bookbrew.bff.web.dto.ForgotPasswordRequestDTO;
 import com.bookbrew.bff.web.dto.LoginRequestDTO;
+import com.bookbrew.bff.web.dto.RecoverEmailRequestDTO;
 import com.bookbrew.bff.web.dto.ResetPasswordRequestDTO;
 import com.bookbrew.bff.web.dto.TokenResponseDTO;
 import com.bookbrew.bff.web.dto.UserResponseDTO;
 
 @FeignClient(name = "auth-service", url = "${auth.service.url}")
 public interface AuthServiceClient {
-    
+
     @PostMapping("/api/auth/login")
     ResponseEntity<UserResponseDTO> login(@RequestBody LoginRequestDTO request);
-    
+
     @PostMapping("/api/auth/forgot-password")
     ResponseEntity<TokenResponseDTO> forgotPassword(@RequestBody ForgotPasswordRequestDTO request);
-    
+
     @PostMapping("/api/auth/reset-password")
     ResponseEntity<Void> resetPassword(@RequestBody ResetPasswordRequestDTO request);
+
+    @PostMapping("/api/auth/recover-email")
+    ResponseEntity<UserResponseDTO> recoverEmail(@RequestBody RecoverEmailRequestDTO request);
 }
---
+++ src/main/java/com/bookbrew/bff/web/dto/RecoverEmailRequestDTO.java
@@ -0,0 +1,22 @@
+package com.bookbrew.bff.web.dto;
+
+public class RecoverEmailRequestDTO {
+    private String cpf;
+    private String email;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
